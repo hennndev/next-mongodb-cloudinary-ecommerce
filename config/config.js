92d8005ba8f20bbb33ddba82bc28f5c1c279857d
hennndev <hendraputra230897@gmail.com>
2022-01-16 22:19:50 +0700

    bug-fixed

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const apiRoute = dev ? 'http://localhost:3000' : 'https://next-mongodb-cloudinary-ecommerce.vercel.app';
+export const apiRoute = dev ? 'http://localhost:3000' : 'https://next-mongodb-cloudinary-ecommerce-96y14rg8c.vercel.app';
